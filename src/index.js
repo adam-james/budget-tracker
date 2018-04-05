207e086ebc86269f6fdb52a2b0390d4ddc64f939
adam-james <adam.j.sulewski@gmail.com>
2018-04-04 20:23:56 -0600

    fix build

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Root from './containers/root'
 import configureStore from './store/configure-store'
 
-const store = configureStore({ expenses })
+const store = configureStore()
 
 ReactDOM.render(
   <Router>
