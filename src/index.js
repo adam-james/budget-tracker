eb4e7042caebe7d964ef72d3aa8aac164e7ea98d
adam-james <adam.j.sulewski@gmail.com>
2018-04-04 20:22:32 -0600

    delete some uneeded stuff

--- src/index.js
+++ src/index.js
@@ -5,33 +5,6 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Root from './containers/root'
 import configureStore from './store/configure-store'
 
-const expenses = fromJS([
-  {
-    id: 1,
-    date: '03/31/2018',
-    description: 'Coffee',
-    amount: 2.25
-  },
-  {
-    id: 2,
-    date: '04/01/2018',
-    description: 'Rent',
-    amount: 1500.00
-  },
-  {
-    id: 3,
-    date: '04/02/2018',
-    description: 'Electric',
-    amount: 19.00
-  },
-  {
-    id: 4,
-    date: '04/05/2018',
-    description: 'Tea',
-    amount: 2.00
-  }
-])
-
 const store = configureStore({ expenses })
 
 ReactDOM.render(
